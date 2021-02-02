379fc29e8b8d73a7a9bac6e4ee8f77af437b3c36
Rinaldi55 <rinaldi207@gmail.com>
2021-02-02 22:22:11 +0700

    Mengubah README.md

---
+++ BelajarJS/looping/looping1.js
@@ -0,0 +1,71 @@
+// var flag = 2;
+// while(flag <=20){
+//     console.log(flag + ' I love coding');
+//     flag=flag+2
+
+// }
+// var i =1;
+// for(i; i <=20; i+=2){
+// console.log(i, "I love coding")
+// }
+// var i = 1;
+// console.log(++i)
+ 
+
+// var flag = 20;
+// while(flag >= 2) { // Loop akan terus berjalan selama nilai flag masih dibawah 10
+//   console.log(flag + ' - I will become a mobile developer'); // Menampilkan nilai flag pada iterasi tertentu
+//   flag=flag-2; // Mengubah nilai flag dengan menambahkan 1
+// }
+
+
+
+var i = 1;
+for(i; i <=20; i++){
+    if(i%2 == 0){
+        console.log(i, "genap");
+    }
+    else if (i%3 == 0){
+        console.log(i, "i love coding");
+    }
+    else if (i%1 == 0){
+    console.log(i, "ganjil");
+    }
+}
+
+// var i = 1;
+
+// for(i; i <=20; i++){
+//     if (i % 2 == 0) {
+    
+//         console.log (i, "genap");
+//     }else if(i % 3 == 0){
+//         console.log(i, "i love coding");
+//     }else if(i % 1 == 0){
+//          console.log(i, "ganji");
+//         } 
+//     }
+// var flag = 1;
+// while(flag <= 4) { // Loop akan terus berjalan selama nilai flag masih dibawah 10
+//   console.log('########'); // Menampilkan nilai flag pada iterasi tertentu
+//   flag++; // Mengubah nilai flag dengan menambahkan 1
+// }
+
+// let str = '   ';
+
+// for(i = 0; i <= 5 ; i++) {
+//     for(j = 1; j <= i; j++) {
+//        str += " * ";
+//     }
+//     str += '\n';
+// }
+// console.log(str);
+// let str ='';
+
+// for(i = 0; i <= 5 ; i++) {
+//     for(j = 1; j <= i; j++) {
+//        str += "#";
+//     }
+//     str += '\n';
+// }
+// console.log(str);
